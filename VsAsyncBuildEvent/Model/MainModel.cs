61492a830b40b7191eef25f2021d68943b21d2dc
cxwl3sxl <xinglin.sc@outlook.com>
2018-07-24 11:38:16 +0800

    增加模式选择功能

--- VsAsyncBuildEvent/Model/MainModel.cs
+++ VsAsyncBuildEvent/Model/MainModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -9,10 +11,15 @@ namespace VsAsyncBuildEvent.Model
     public class MainModel : INotifyPropertyChanged
     {
         private string _message;
+        private int _delay = 5;
+        private bool _runImmediately;
+        private bool _runDelay;
+        private bool _runManually;
         public ObservableCollection<BuildProcess> AllBuildProcesses { get; } = new ObservableCollection<BuildProcess>();
         public ICommand SuspendCommand { get; }
         public ICommand ShowProcess { get; }
         public ICommand HideProcess { get; }
+        public ICommand StartCommand { get; }
 
         public string Message
         {
@@ -24,16 +31,66 @@ namespace VsAsyncBuildEvent.Model
             }
         }
 
+        public int Delay
+        {
+            get => _delay;
+            set
+            {
+                _delay = value;
+                OnPropertyChanged(nameof(Delay));
+            }
+        }
+
+        public bool RunImmediately
+        {
+            get => _runImmediately;
+            set
+            {
+                _runImmediately = value;
+                OnPropertyChanged(nameof(RunImmediately));
+            }
+        }
+
+        public bool RunDelay
+        {
+            get => _runDelay;
+            set
+            {
+                _runDelay = value;
+                OnPropertyChanged(nameof(RunDelay));
+            }
+        }
+
+        public bool RunManually
+        {
+            get => _runManually;
+            set
+            {
+                _runManually = value;
+                OnPropertyChanged(nameof(RunManually));
+            }
+        }
+
         public MainModel()
         {
             SuspendCommand = new RealCommand<BuildProcess>(Suspend);
             ShowProcess = new RealCommand<BuildProcess>(p => p?.Show());
-            HideProcess = new RealCommand<BuildProcess>(p => p.Hide());
+            HideProcess = new RealCommand<BuildProcess>(p => p?.Hide());
+            StartCommand = new RealCommand<BuildProcess>(p =>
+            {
+                if (p == null) return;
+                p.Start();
+                Message = $"正在执行 {p.Cmd} {p.Argument}";
+            });
             IsServiceOk = false;
+            RunImmediately = true;
+            RunDelay = false;
+            RunManually = false;
         }
 
         void Suspend(BuildProcess process)
         {
+            if (process == null) return;
             process.Stop();
             AllBuildProcesses.Remove(process);
         }
@@ -41,10 +98,20 @@ namespace VsAsyncBuildEvent.Model
         public void Build(string cmd, string arguments)
         {
             var buildProcess = new BuildProcess(cmd, arguments) { BuildFinfished = BuildProcessFinished };
-            buildProcess.Start();
+            var msg = $"正在执行 {cmd} {arguments}";
+            if (MainModelResover.StaticMainModel.RunImmediately)
+                buildProcess.Start();
+            else
+                msg = $"任务已经添加 {cmd} {arguments}";
+
+            if (MainModelResover.StaticMainModel.RunDelay)
+            {
+                Task.Factory.StartNew(DelayRun, buildProcess);
+            }
+
             Application.Current.Dispatcher.Invoke(new Action<BuildProcess>(bp => AllBuildProcesses.Add(bp)),
                 buildProcess);
-            Message = $"正在执行 {cmd} {arguments}";
+            Message = msg;
         }
 
         void BuildProcessFinished(BuildProcess process)
@@ -61,6 +128,14 @@ namespace VsAsyncBuildEvent.Model
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        void DelayRun(object bpObject)
+        {
+            if (!(bpObject is BuildProcess buildProcess)) return;
+            Thread.Sleep(MainModelResover.StaticMainModel.Delay * 1000);
+            buildProcess.Start();
+            Message = $"正在执行 {buildProcess.Cmd} {buildProcess.Argument}";
         }
     }
 }
